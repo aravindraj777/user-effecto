43befa85767fbdd2696e3fefa81ffbab5852de1b
Aravind <aravindmunna240@gmail.com>
2024-02-18 23:18:33 +0530

    Initial commit

---
+++ src/test/java/com/management/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
